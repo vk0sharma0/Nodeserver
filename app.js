8212e7301ad24942c6b44473327cd56c3e0396be
vishal kumar sharma <136582084+vk0sharma0@users.noreply.github.com>
2024-08-25 19:00:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 7000;
 
 let jsonData;
 
